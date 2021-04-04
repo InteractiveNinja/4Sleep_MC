f4c50ebbd30cc2a1a29c63049b4c956166d22a3f
InteractiveNinja <gabrielnadolny2@gmail.com>
2021-04-04 16:14:13 +0200

    added that you can sleep if it is raining

--- src/eu/imninja/sleep/Listener.java
+++ src/eu/imninja/sleep/Listener.java
@@ -14,12 +14,10 @@ import org.bukkit.plugin.Plugin;
 import java.util.ArrayList;
 
 public class Listener implements org.bukkit.event.Listener {
-    private Plugin p = null;
-    private World w = null;
-    private long beforeNight = 12541;
-    private long afterNight = 23458;
+    private Plugin p;
+    private World w;
     private ArrayList<String> playersSleeping = new ArrayList<String>();
-    private FileConfiguration config = null;
+    private FileConfiguration config;
 
 
     private boolean playSoundAfterSleep = true;
@@ -30,14 +28,14 @@ public class Listener implements org.bukkit.event.Listener {
     private String titleSmall = "Day {time}";
     private String textCantSleepAtNight = "You only can sleep at Night time";
     private String textWantsToSleep = "{playername} wants to sleep, {missing} zzZZzzzzZ";
-    private String textDoesntSleepMore =  "{playername} woke up, {missing}";
+    private String textDoesntSleepMore = "{playername} woke up, {missing}";
 
     private int minimalSleep = -1;
 
 
     public Listener(Plugin p, FileConfiguration config) {
         this.p = p;
-        this.w  = p.getServer().getWorld("world");
+        this.w = p.getServer().getWorld("world");
         this.config = config;
         setValues();
     }
@@ -47,17 +45,16 @@ public class Listener implements org.bukkit.event.Listener {
             playSoundAfterSleep = (boolean) config.get("soundAfterSleep");
             showTitle = (boolean) config.get("showTitle");
             showDayCounter = (boolean) config.get("showDayCounter");
-            minimalSleep = (int)config.get("minimalSleep");
-            titleBig = (String)config.get("titleTextBig");
-            titleSmall = (String)config.get("titleTextSmall");
-            textCantSleepAtNight = (String)config.get("textCantSleepAtNight");
-            textWantsToSleep = (String)config.get("textWantsToSleep");
-            textDoesntSleepMore = (String)config.get("textDoesntSleepMore");
+            minimalSleep = (int) config.get("minimalSleep");
+            titleBig = (String) config.get("titleTextBig");
+            titleSmall = (String) config.get("titleTextSmall");
+            textCantSleepAtNight = (String) config.get("textCantSleepAtNight");
+            textWantsToSleep = (String) config.get("textWantsToSleep");
+            textDoesntSleepMore = (String) config.get("textDoesntSleepMore");
 
 
-
-        }catch (ClassCastException e) {
-            System.out.println(Main.tag +" Casting Error, please see the Config, defaults will be used");
+        } catch (ClassCastException e) {
+            System.out.println(Main.tag + " Casting Error, please see the Config, defaults will be used");
             playSoundAfterSleep = true;
             showTitle = true;
             showDayCounter = true;
@@ -66,7 +63,7 @@ public class Listener implements org.bukkit.event.Listener {
             titleSmall = "Day {time}";
             textCantSleepAtNight = "You only can sleep at Night time";
             textWantsToSleep = "{playername} wants to sleep, {missing} zzZZzzzzZ";
-            textDoesntSleepMore =  "{playername} woke up, {missing}";
+            textDoesntSleepMore = "{playername} woke up, {missing}";
         }
 
     }
@@ -75,37 +72,35 @@ public class Listener implements org.bukkit.event.Listener {
     private void onPlayerSleep(PlayerBedEnterEvent e) {
         String playerName = e.getPlayer().getDisplayName();
 
-        if(isNight()) {
+        if (isNight() || isRaining()) {
             playersSleeping.add(playerName);
-            String message = textWantsToSleep.replace("{playername}",ChatColor.AQUA + e.getPlayer().getDisplayName() + ChatColor.GOLD).replace("{missing}",showMissing());
+            String message = textWantsToSleep.replace("{playername}", ChatColor.AQUA + e.getPlayer().getDisplayName() + ChatColor.GOLD).replace("{missing}", showMissing());
             p.getServer().broadcastMessage(message);
+            if (isEnough()) wakeUp();
 
-            if(isEnough()) {
-               wakeUp();
-
-            }
         } else {
-            e.setCancelled(true);
-            String message = ChatColor.AQUA+ Main.tag + ChatColor.GOLD + textCantSleepAtNight;
-            e.getPlayer().sendMessage( message);
+
+            String message = ChatColor.AQUA + Main.tag + ChatColor.GOLD + textCantSleepAtNight;
+            e.getPlayer().sendMessage(message);
         }
+        e.setCancelled(true);
     }
 
     @EventHandler
     private void onPlayerStopSleep(PlayerBedLeaveEvent e) {
         String playerName = e.getPlayer().getDisplayName();
         playersSleeping.remove(playerName);
-        if(isNight()){
-            String message = textDoesntSleepMore.replace("{playername}",ChatColor.AQUA + e.getPlayer().getDisplayName() + ChatColor.GRAY).replace("{missing}",showMissing());
-            p.getServer().broadcastMessage( message);
+        if (isNight()) {
+            String message = textDoesntSleepMore.replace("{playername}", ChatColor.AQUA + e.getPlayer().getDisplayName() + ChatColor.GRAY).replace("{missing}", showMissing());
+            p.getServer().broadcastMessage(message);
         }
     }
 
     @EventHandler
     private void onPlayerQuits(PlayerQuitEvent e) {
         playersSleeping.remove(e.getPlayer().getDisplayName());
-        if(playersSleeping.size() < 0) {
-            if(isNight() && isEnough()) {
+        if (playersSleeping.size() < 0) {
+            if (isNight() && isEnough()) {
                 wakeUp();
             }
         }
@@ -114,51 +109,50 @@ public class Listener implements org.bukkit.event.Listener {
 
     private boolean isNight() {
         long time = w.getTime();
-        if( time >= beforeNight && time <= afterNight) {
-            return true;
-        } else {
-            return false;
-        }
+        long beforeNight = 12541;
+        long afterNight = 23458;
+        return (time >= beforeNight && time <= afterNight);
+    }
+
+    private boolean isRaining() {
+        return w.hasStorm();
     }
 
     private boolean isEnough() {
-        int needToSleep = (minimalSleep == -1) ? getPlayersOnline()/2 : minimalSleep;
-            if(needToSleep <= getPlayersSleeping()) {
-                return true;
-            } else {
-                return false;
-            }
-
-
+        int needToSleep = (minimalSleep == -1) ? getPlayersOnline() / 2 : minimalSleep;
+        return (needToSleep <= getPlayersSleeping());
     }
 
     private int getPlayersOnline() {
         return p.getServer().getOnlinePlayers().size();
     }
+
     private int getPlayersSleeping() {
-       return playersSleeping.size();
+        return playersSleeping.size();
     }
 
     private String showMissing() {
-        int online = (minimalSleep == -1)? getPlayersOnline() : minimalSleep;
-        int sleeping =  getPlayersSleeping();
+        int online = (minimalSleep == -1) ? getPlayersOnline() : minimalSleep;
+        int sleeping = getPlayersSleeping();
 
         return sleeping + "/" + online;
     }
 
     private void wakeUp() {
-        playersSleeping = new ArrayList<String>();
+        playersSleeping.clear();
         w.setTime(0L);
-        p.getServer().getOnlinePlayers().forEach(player ->{
+        w.setThundering(false);
+        w.setStorm(false);
+        p.getServer().getOnlinePlayers().forEach(player -> {
             sendTitle(player);
         });
     }
 
-    private void sendTitle(Player p){
-        String smallTitle = (showDayCounter)? ChatColor.GOLD+titleSmall.replace("{time}", ""+(w.getFullTime()/24000)): "";
-        String bigTitle = ChatColor.GOLD+titleBig;
-        if(showTitle)p.sendTitle(bigTitle,smallTitle);
-        if(playSoundAfterSleep)p.playSound(p.getLocation(), Sound.UI_TOAST_CHALLENGE_COMPLETE,1f,1f);
+    private void sendTitle(Player p) {
+        String smallTitle = (showDayCounter) ? ChatColor.GOLD + titleSmall.replace("{time}", "" + (w.getFullTime() / 24000)) : "";
+        String bigTitle = ChatColor.GOLD + titleBig;
+        if (showTitle) p.sendTitle(bigTitle, smallTitle);
+        if (playSoundAfterSleep) p.playSound(p.getLocation(), Sound.UI_TOAST_CHALLENGE_COMPLETE, 1f, 1f);
     }
 
 }
